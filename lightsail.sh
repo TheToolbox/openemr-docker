351c5d56073dfadd125f51fbdac9fe407a8d9438
Asher Densmore-Lynn <jesdynf@gmail.com>
2017-08-31 19:53:02 -0500

    typo

--- lightsail.sh
+++ lightsail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L https://github.com/jesdynf/openemr-docker > lightsail-launch.sh
+curl -L https://github.com/jesdynf/openemr-docker/lightstail-launch.sh > lightsail-launch.sh
 chmod +x lightsail-launch.sh
 sudo ./lightsail-launch.sh
