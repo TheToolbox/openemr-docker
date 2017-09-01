431b1a7a9667499da05680fdc5726458381848e7
Asher Densmore-Lynn <jesdynf@gmail.com>
2017-08-31 19:52:28 -0500

    opening hand

---
+++ README.md
@@ -0,0 +1,5 @@
+# OpenEMR Docker for LightSail
+
+https://davekz.com/docker-on-lightsail/
+
+Base OS, "Ubuntu 16.04", paste lightsail.sh, go
---
+++ lightsail.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -L https://github.com/jesdynf/openemr-docker > lightsail-launch.sh
+chmod +x lightsail-launch.sh
+sudo ./lightsail-launch.sh
